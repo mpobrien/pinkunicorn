f96f26f2761a1d8fcb414fd12a4429533d2d5ef4
Kasper Overgård Nielsen <kasper@byolimit.com>
2022-03-10 23:24:58 +0100

    Drawing tools fully functional

--- xamarin/PinkUnicorn/ViewModels/DrawingVievModel.cs
+++ xamarin/PinkUnicorn/ViewModels/DrawingVievModel.cs
@@ -16,10 +16,10 @@ namespace PinkUnicorn.ViewModels
         private IQueryable<Component> components;
         public IQueryable<Component> Components { get => components; }
 
-        private SKColor strokeColor = SKColors.Black;
+        private SKColor strokeColor;
         public SKColor StrokeColor { get => strokeColor; set => SetAndRaise(ref strokeColor, value); }
 
-        private float strokeWidth = 1;
+        private float strokeWidth;
         public float StrokeWidth { get => strokeWidth; set => SetAndRaise(ref strokeWidth, value); }
 
         private SKColor? fillColor = null;
@@ -28,16 +28,24 @@ namespace PinkUnicorn.ViewModels
         private bool enableFill;
         public bool EnableFill { get => enableFill; set => SetAndRaise(ref enableFill, value); }
 
-        public IEnumerable<Shape> SupportedShapes => new[] { Shape.Circle, Shape.Path, Shape.Rectangle };
-        private Shape shape = Shape.Circle;
+        private Shape shape;
         public Shape Shape { get => shape; set => SetAndRaise(ref shape, value); }
+
+        public IEnumerable<Shape> SupportedShapes => new[] { Shape.Circle, Shape.Path, Shape.Rectangle };
 
         private readonly List<IDisposable> subscriptions = new();
 
         public DrawingVievModel(Realm realm)
         {
+            // Set defaults
+            StrokeColor = SKColors.DarkGray;
+            StrokeWidth = 1;
+            FillColor = null;
+            EnableFill = false;
+            Shape = SupportedShapes.First();
+
             this.realm = realm;
-            components = realm.All<Component>();
+            components = realm.All<Component>().OrderBy(c => c.Z).ThenBy(c => c.Id); // deterministic ordering
 
             subscriptions.AddRange(new[] {
                 components.AsRealmCollection().SubscribeForNotifications((sender, changes, error) => RaisePropertyChanged(() => Components)),
